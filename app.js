633f2b627ce89c44066f4619091c0cf9b5b46291
Nazier Chand <66347003+NazierC@users.noreply.github.com>
2021-11-10 12:05:06 +1100

    Initial

---
+++ app.js
@@ -0,0 +1,9 @@
+var text = Document.getElementsByClassName('text')
+var send = Document.getElementsByClassName('send')
+ 
+send.addEventListener("click", function(e) {
+    if(text.value != " " || text.value != "")
+    {
+                  
+    }
+})
