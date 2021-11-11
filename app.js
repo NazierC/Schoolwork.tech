f6d7ddfc414a9dcf767579623a6e5bb0ca5521e4
Nazier Chand <66347003+NazierC@users.noreply.github.com>
2021-11-11 14:56:38 +1100

    j

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var text = Document.getElementsByClassName('text')
-var send = Document.getElementsByClassName('send')
+var text = document.getElementById('text');
+var send = document.getElementById('send');
  
-send.addEventListener("click", function(e) {
+send.addEventListener("click", function() {
     if(text.value != " " || text.value != "")
     {
-                  
+        alert('hi');
+
     }
 })
---
+++ server.js
@@ -0,0 +1,16 @@
+const io = require("socket.io")();
+var fs = require('fs');
+var crypto = require('crypto');
+var newFN;
+
+io.on("connection", socket => {
+    socket.on("Create" (data) =>{
+        newFN = "0x" + crypto.randomBytes(32).toString('hex');
+        console.log(newFN);
+        fs.writeFile(`../${newFN}`, data, function(err){
+            if (err){
+                 console.log(err);
+            }
+        })     
+    });
+}); 
